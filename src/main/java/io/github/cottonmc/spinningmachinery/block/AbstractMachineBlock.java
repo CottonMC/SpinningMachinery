a2d2733a92154ed6b34cb42282681ffc6ffaa34e
Juuxel <kasperi.kauppi@gmail.com>
2019-06-09 23:09:27 +0300

    Drop machine contents when destroyed

--- src/main/java/io/github/cottonmc/spinningmachinery/block/AbstractMachineBlock.java
+++ src/main/java/io/github/cottonmc/spinningmachinery/block/AbstractMachineBlock.java
@@ -1,8 +1,6 @@
 package io.github.cottonmc.spinningmachinery.block;
 
 import io.github.cottonmc.spinningmachinery.block.entity.AbstractMachineBlockEntity;
-import io.github.cottonmc.spinningmachinery.block.entity.GrinderBlockEntity;
-import io.github.cottonmc.spinningmachinery.gui.SpinningGuis;
 import io.github.cottonmc.spinningmachinery.util.FunctionUtils;
 import net.fabricmc.fabric.api.container.ContainerProviderRegistry;
 import net.minecraft.block.Block;
@@ -11,6 +9,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateFactory;
 import net.minecraft.state.property.BooleanProperty;
@@ -18,6 +17,7 @@ import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -66,5 +66,19 @@ public abstract class AbstractMachineBlock extends BlockWithEntity {
     @Override
     public BlockRenderType getRenderType(BlockState state) {
         return BlockRenderType.MODEL;
+    }
+
+    @Override
+    public void onBlockRemoved(BlockState state, World world, BlockPos pos, BlockState newState, boolean b) {
+        if (state.getBlock() != newState.getBlock()) {
+            BlockEntity be = world.getBlockEntity(pos);
+
+            if (be instanceof Inventory) {
+                ItemScatterer.spawn(world, pos, (Inventory) be);
+                world.updateHorizontalAdjacent(pos, this);
+            }
+
+            super.onBlockRemoved(state, world, pos, newState, b);
+        }
     }
 }
